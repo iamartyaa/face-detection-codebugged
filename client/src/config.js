5be62dba61d688f1722d5e1b2bb5bea0a5757395
Simran Yadav <200108060@hbtu.ac.in>
2024-04-01 20:06:50 +0530

    4th commit

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:3001';
+const BASE_URL = 'https://face-detection-codebugged-2.onrender.com';
 // const BASE_URL = 'https://face-detection-backend-08wo.onrender.com';
 
 export default BASE_URL;
